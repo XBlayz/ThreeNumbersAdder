6e7eece0c951afcc601c39b902e29216039fe050
XBlayz <stefano.scarcelli15@gmail.com>
2023-12-12 16:17:47 +0100

    Update Sim_Add.vhd
    + Filled file description;
    ~ Syntax clean up;
    ~ Fix timing;

--- Vivado_project_v1/Vivado_project_v1.srcs/sim_1/new/Sim_Add.vhd
+++ Vivado_project_v1/Vivado_project_v1.srcs/sim_1/new/Sim_Add.vhd
@@ -1,20 +1,20 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: 
+-- Engineer: Stefano Scarcelli, Michele De Fusco
 -- 
 -- Create Date: 12.12.2023 11:20:24
--- Design Name: 
+-- Design Name: ---
 -- Module Name: Sim_Add - Version1
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
+-- Project Name: ThreeNumbersAdder
+-- Target Devices: xc7z020clg400-2
+-- Tool Versions: 2023.2
+-- Description: Main simulation
 -- 
--- Dependencies: 
+-- Dependencies: Three_Adder.vhd
 -- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- Revision: 1
+-- Revision 1.0 - Implementation
+-- Additional Comments: ---
 -- 
 ----------------------------------------------------------------------------------
 
@@ -23,21 +23,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_arith.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Sim_Add is
     generic (n : integer := 8);
 end Sim_Add;
 
 architecture Version1 of Sim_Add is
-
     component Three_Adder
         generic (n : integer := 8);
         Port (A : in STD_LOGIC_VECTOR (n-1 downto 0);
@@ -47,29 +37,29 @@ architecture Version1 of Sim_Add is
               CLK : in STD_LOGIC;
               Clear : in STD_LOGIC);
     end component;
-    Signal IA,IB,IC : STD_LOGIC_VECTOR (n-1 downto 0);
+    Signal IA, IB, IC : STD_LOGIC_VECTOR (n-1 downto 0);
     Signal ORR : STD_LOGIC_VECTOR (n+1 downto 0);
     signal clk, clear : STD_LOGIC;
-    constant T : time := 5 ms;
+    constant T : time := 10 ns;
 begin
-    TA : Three_Adder port map(IA,IB,IC,ORR,clk,clear);
+    TA : Three_Adder port map(IA, IB, IC, ORR, clk, clear);
     process begin
-    clk <= '0';
-    wait for T/2;
-    clk <= '1';
-    wait for T/2;
+        clk <= '0';
+        wait for T/2;
+        clk <= '1';
+        wait for T/2;
     end process;
+
     process begin
-    clear <= '1';
-    wait until falling_edge(clk);
-    clear <= '0';
-    
-    wait for 4 ms;
-    
-    IA<=(others=>'1');
-    IB<=(others=>'1');
-    IC<=(others=>'1');
-    wait for T;
-    
+        clear <= '1';
+        wait for T;
+        clear <= '0';
+        
+        wait for T/4;
+        IA <= (others=>'1');
+        IB <= (others=>'1');
+        IC <= (others=>'1');
+        wait for 3*T;
     end process;
+
 end Version1;
