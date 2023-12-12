c3011b4bdb2a40a640c573df934f30ef9b4655d6
XBlayz <stefano.scarcelli15@gmail.com>
2023-12-12 15:53:55 +0100

    Update Three_Adder.vhd
    ~ Three_Adder.vhd Syntax;

--- Vivado_project_v1/Vivado_project_v1.srcs/sources_1/new/Three_Adder.vhd
+++ Vivado_project_v1/Vivado_project_v1.srcs/sources_1/new/Three_Adder.vhd
@@ -13,7 +13,7 @@
 -- Dependencies: Register_n.vhd, Synched_adder.vhd
 -- 
 -- Revision: 1
--- Revision 0.01 - File Created
+-- Revision 1.0 - Implementation
 -- Additional Comments: ---
 -- 
 ----------------------------------------------------------------------------------
@@ -35,8 +35,7 @@ end Three_Adder;
 architecture Version1 of Three_Adder is
     component Synched_adder
         generic (n : integer := 8);
-        Port (
-              A, B : in STD_LOGIC_VECTOR (n-1 downto 0);
+        Port (A, B : in STD_LOGIC_VECTOR (n-1 downto 0);
               R : out STD_LOGIC_VECTOR (n downto 0);
               CLK, Clear : in STD_LOGIC);
     end component;
@@ -55,14 +54,12 @@ architecture Version1 of Three_Adder is
     signal Rs,RCext: STD_LOGIC_VECTOR (n downto 0);
     signal RSF: STD_LOGIC_VECTOR (n+1 downto 0);
 begin
+    RCext <= Rc(n-1) & Rc;
     RegC: Register_n generic map(n) port map(CLK, Clear, C, Rc);
     
-    SA1:  Synched_adder generic map(n) port map(A,B,Rs,CLK,Clear);
-    
-    RCext<=Rc(n-1) & Rc;
-    SA2:  Synched_adder generic map(n+1) port map(Rs,RCext,RSF,CLK,Clear);
+    SA1:  Synched_adder generic map(n) port map(A, B, Rs, CLK, Clear);
+    SA2:  Synched_adder generic map(n+1) port map(Rs, RCext, RSF, CLK, Clear);
     
     RegSF:Register_n generic map(n+2) port map(CLK, Clear, RSF, R);
-
 
 end Version1;
