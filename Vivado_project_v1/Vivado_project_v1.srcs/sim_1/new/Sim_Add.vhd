dfde1af047b4cce24347df70f6207525fb9a42e8
Mikidefu <mikidefu.mdf@gmail.com>
2023-12-13 13:49:52 +0100

    Update Sim_Add.vhd

--- Vivado_project_v1/Vivado_project_v1.srcs/sim_1/new/Sim_Add.vhd
+++ Vivado_project_v1/Vivado_project_v1.srcs/sim_1/new/Sim_Add.vhd
@@ -29,7 +29,7 @@ end Sim_Add;
 
 architecture Version1 of Sim_Add is
     component Three_Adder
-        generic (n : integer := 8);
+        --generic (n : integer := 8);
         Port (A : in STD_LOGIC_VECTOR (n-1 downto 0);
               B : in STD_LOGIC_VECTOR (n-1 downto 0);
               C : in STD_LOGIC_VECTOR (n-1 downto 0);
@@ -55,11 +55,37 @@ begin
         wait for T;
         clear <= '0';
         
+        
+        -- (-1)+(-1)+(-1)=-3
+        
         wait for T/4;
         IA <= (others=>'1');
         IB <= (others=>'1');
         IC <= (others=>'1');
         wait for 3*T;
+        
+        -- 127+127+127=381
+        
+        wait for T/4;
+        IA(n-1) <= ('0');
+        IA(n-2 downto 0) <= (others=>'1');
+        IB(n-1) <= ('0');
+        IB(n-2 downto 0) <= (others=>'1');
+        IC(n-1) <= ('0');
+        IC(n-2 downto 0) <= (others=>'1');
+        wait for 3*T;
+        
+        -- (-64)+1+(-63)=0
+        
+        wait for T/4;
+        IA(n-1 downto n-2) <= (others=>'0');
+        IA(n-3 downto 0) <= (others=>'1');
+        IB(n-1 downto 1) <= (others=>'0');
+        IB(0) <= ('1');
+        IC(n-1 downto n-2) <= (others=>'1');
+        IC(n-3 downto 0) <= (others=>'0');
+        wait for 3*T;
+        
     end process;
 
 end Version1;
