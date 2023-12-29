af99a9ecf80497bfacf8ad5a4edf3b2a400c0894
Mikidefu <mikidefu.mdf@gmail.com>
2023-12-29 13:19:26 +0100

    Update Three_Adder.vhd

--- Vivado_project_v2/Vivado_project_v2.srcs/sources_1/new/Three_Adder.vhd
+++ Vivado_project_v2/Vivado_project_v2.srcs/sources_1/new/Three_Adder.vhd
@@ -41,13 +41,39 @@ architecture Version2 of Three_Adder is
     end component;
     signal Ra, Rb, Rc: STD_LOGIC_VECTOR (n-1 downto 0);
     signal Rs: STD_LOGIC_VECTOR (n downto 0);
+    
+    component FullAdder
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           c : in STD_LOGIC;
+           cout : out STD_LOGIC;
+           r : out STD_LOGIC);
+    end component;
+    
+    component Adder is
+    generic (n: integer :=8);
+    Port ( clk : in STD_LOGIC;
+           clear : in STD_LOGIC;
+           a : in STD_LOGIC_VECTOR (n-1 downto 0);
+           b : in STD_LOGIC_VECTOR (n-1 downto 0);
+           r : out STD_LOGIC_VECTOR (n downto 0));
+    end component;
+    
+    signal vr, sp: STD_LOGIC_VECTOR(n downto 0);
+    signal a1, b1, c1, vr1, sp1: STD_LOGIC_VECTOR(n-1 downto 0);
 begin
     RegA: Register_n generic map(n) port map(CLK, Clear, A, Ra);
     RegB: Register_n generic map(n) port map(CLK, Clear, B, Rb);
     RegC: Register_n generic map(n) port map(CLK, Clear, C, Rc);
     
     -- DA FARE: Circuito di somma (Ra+Rb+Rc=Rs)
+    s1: for i in 0 to (n-1) generate
+        FAi: FullAdder port map(a1(i), b1(i), c1(i), vr(i), sp(i));
+    end generate;
     
-    RegS: Register_n generic map(n+2) port map(CLK, Clear, Rs, R);
+    sp <= sp1(n-1) & sp1;
+    vr <= vr1 & '0';
+    
+    Sum: Adder generic map(n+1) port map(CLK, Clear, vr, sp, R);
 
 end Version2;
